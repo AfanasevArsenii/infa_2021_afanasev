6c60e8fdd2ead810d958a717490c7ee7ac3811c0
AfanasevArsenii <afanasev.arsenii@phystech.edu>
2021-10-05 01:23:04 +0300

    Кошка без лап.

--- 4.2.py
+++ 4.2.py
@@ -32,7 +32,7 @@ rect(screen, (255, 255, 240), (0, 0, 400, 250))
 
 #circle(screen, (230, 230, 250), (130, 300), 100, draw_top_right = True, draw_top_left = True)
 ellipse(tent_color_up, (224, 255, 255), (0, 0, 200, 250))
-ellipse(tent_color_down, (191, 239, 255), (0, 0, 200, 100))
+ellipse(tent_color_down, (224, 255, 255), (0, 0, 200, 100))
 arc(tent, (25, 25, 112), (0, 100, 200, 250), 0, math.pi, 3)
 arc(tent, (25, 25, 112), (0, 165, 200, 100), math.pi, 2*math.pi, 3)
 arc(tent, (25, 25, 112), (10, 120, 180, 100), math.pi, 2*math.pi, 3)
@@ -51,15 +51,15 @@ rect(coat, (255, 211, 155), (45, 0, 10, 90), border_radius=5)
 ellipse(man, (255, 211, 155), (60, 1, 80, 70), 0) # капюшон пух
 ellipse(man, (205, 170, 125), (65, 10, 70, 60), 0) # капюшон
 ellipse(man, (255, 255, 224), (75, 20, 50, 40), 0) # лицо
-
+line(man, (0, 0, 0), (95, 35), (85, 30), 2) # левый глаз
+line(man, (0, 0, 0), (105, 35), (115, 30), 2) # правый глаз
+arc(man, (0, 0, 0), (93, 45, 15, 10), 0, math.pi, 2) #рот
 #левая рука
 ellipse(left_arm, (205, 170, 125), (0, 0, 60, 15), 0)
-left_arm = pygame.transform.rotate(left_arm, -30)
-man.blit(left_arm, (15, 45))
+left_arm = pygame.transform.rotate(left_arm, -60)
 #правая рука
 ellipse(right_arm, (205, 170, 125), (0, 0, 60, 15), 0)
 right_arm = pygame.transform.rotate(right_arm, -30)
-right_arm = pygame.transform.scale(right_arm, (120, 30))
 # левый валенок
 rect(man, (205, 170, 125), (75, 135, 20, 30), border_radius=5)
 rect(man, (205, 170, 125), (105, 145, 30, 20), border_radius=10)
@@ -71,8 +71,15 @@ rect(man, (205, 170, 125), (65, 145, 20, 20), border_radius=10)
 line(man, (0, 0, 0), (30,40), (20, 160), 2)
 
 #кошка
-ellipse(cat, (255, 130, 71), (60, 70, 90, 30))
-ellipse(cat, (255, 130, 71), (45, 51, 35, 30))
+ellipse(cat, (255, 130, 71), (60, 70, 90, 30)) #тело
+ellipse(cat, (255, 130, 71), (45, 51, 35, 30)) #голова
+ellipse(cat, (255, 255, 255), (50, 60, 10, 5)) #левый глаз
+ellipse(cat, (255, 255, 255), (65, 60, 10, 5)) #правый глаз
+ellipse(cat, (0, 0, 0), (50, 60, 5, 5)) #левый глаз
+ellipse(cat, (0, 0, 0), (65, 60, 5, 5)) #правый глаз
+polygon(cat, (255, 130, 71), [(50,60), (60,50), (45,45)]) #левое ухо
+polygon(cat, (255, 130, 71), [(80, 65), (80,45), (65,50)]) #левое ухо
+ellipse(cat, (0, 0, 0), (58, 67, 5, 5))
 """ellipse(cat_left_arm, (0, 130, 71), (0, 0, 40, 15))
 cat_left_arm = pygame.transform.rotate(cat_left_arm, -60)
 cat.blit(cat_left_arm, (0, 0))
