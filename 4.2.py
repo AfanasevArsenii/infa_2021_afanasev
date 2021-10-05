fb43dab0fde4a306ef56c2eefb83e7a674ecfd27
AfanasevArsenii <afanasev.arsenii@phystech.edu>
2021-10-05 13:37:26 +0300

    Кошка без лап.

--- 4.2.py
+++ 4.2.py
@@ -15,16 +15,16 @@ tent_color_down = pygame.Surface.subsurface(tent, (0, 165, 200, 100))
 
 man = pygame.Surface((200, 200), pygame.SRCALPHA)
 coat = pygame.Surface.subsurface(man, (50, 40, 100, 100))
-left_arm = pygame.Surface.subsurface(man, (20, 70, 60, 15))
-right_arm = pygame.Surface.subsurface(man, (120, 70, 60, 15))
+left_arm = pygame.Surface((60, 15), pygame.SRCALPHA)
+right_arm = pygame.Surface((60, 15), pygame.SRCALPHA)
 
 cat = pygame.Surface((200, 200), pygame.SRCALPHA)
-cat_left_arm = pygame.Surface.subsurface(cat, (60, 70, 40, 15))
-cat_right_arm = pygame.Surface.subsurface(cat, (60, 70, 40, 15))
-cat_left_leg = pygame.Surface.subsurface(cat, (150, 70, 40, 15))
-cat_right_leg = pygame.Surface.subsurface(cat, (160, 70, 40, 15))
-cat_tail = pygame.Surface.subsurface(cat, (60, 70, 30, 5))
-fish = pygame.Surface.subsurface(cat, (20, 20, 100, 100))
+cat_left_arm = pygame.Surface((60, 15), pygame.SRCALPHA)
+cat_right_arm = pygame.Surface((60, 15), pygame.SRCALPHA)
+cat_left_leg = pygame.Surface((60, 15), pygame.SRCALPHA)
+cat_right_leg = pygame.Surface((60, 15), pygame.SRCALPHA)
+cat_tail = pygame.Surface((80, 20), pygame.SRCALPHA)
+fish = pygame.Surface((100, 100), pygame.SRCALPHA)
 
 
 rect(screen, (255, 255, 240), (0, 0, 400, 250))
@@ -56,10 +56,12 @@ line(man, (0, 0, 0), (105, 35), (115, 30), 2) # правый глаз
 arc(man, (0, 0, 0), (93, 45, 15, 10), 0, math.pi, 2) #рот
 #левая рука
 ellipse(left_arm, (205, 170, 125), (0, 0, 60, 15), 0)
-left_arm = pygame.transform.rotate(left_arm, -60)
+left_arm = pygame.transform.rotate(left_arm, -40)
+man.blit(left_arm, (25, 35))
 #правая рука
 ellipse(right_arm, (205, 170, 125), (0, 0, 60, 15), 0)
 right_arm = pygame.transform.rotate(right_arm, -30)
+man.blit(right_arm, (120, 60))
 # левый валенок
 rect(man, (205, 170, 125), (75, 135, 20, 30), border_radius=5)
 rect(man, (205, 170, 125), (105, 145, 30, 20), border_radius=10)
@@ -77,24 +79,42 @@ ellipse(cat, (255, 255, 255), (50, 60, 10, 5)) #левый глаз
 ellipse(cat, (255, 255, 255), (65, 60, 10, 5)) #правый глаз
 ellipse(cat, (0, 0, 0), (50, 60, 5, 5)) #левый глаз
 ellipse(cat, (0, 0, 0), (65, 60, 5, 5)) #правый глаз
-polygon(cat, (255, 130, 71), [(50,60), (60,50), (45,45)]) #левое ухо
-polygon(cat, (255, 130, 71), [(80, 65), (80,45), (65,50)]) #левое ухо
+polygon(cat, (255, 130, 71), [(50, 60), (60, 50), (45, 45)]) #левое ухо
+polygon(cat, (255, 130, 71), [(80, 65), (80, 45), (65, 50)]) #левое ухо
 ellipse(cat, (0, 0, 0), (58, 67, 5, 5))
-"""ellipse(cat_left_arm, (0, 130, 71), (0, 0, 40, 15))
-cat_left_arm = pygame.transform.rotate(cat_left_arm, -60)
-cat.blit(cat_left_arm, (0, 0))
-ellipse(cat_right_arm, (255, 130, 71), (0, 0, 40, 15))
-ellipse(cat_left_leg, (255, 130, 71), (0, 0, 40, 15))
-ellipse(cat_right_leg, (255, 130, 71), (0, 0, 40, 15))
-ellipse(cat_tail, (255, 130, 71), (0, 0, 30, 5))
-"""
+
+ellipse(cat_left_arm, (255, 130, 71), (0, 0, 60, 15))
+cat_left_arm = pygame.transform.rotate(cat_left_arm, 30)
+cat.blit(cat_left_arm, (15, 80))
+ellipse(cat_right_arm, (255, 130, 71), (0, 0, 60, 15))
+cat_right_arm = pygame.transform.rotate(cat_right_arm, 50)
+cat.blit(cat_right_arm, (40, 80))
+
+
+ellipse(cat_left_leg, (255, 130, 71), (0, 0, 60, 15))
+cat_left_leg = pygame.transform.rotate(cat_left_leg, -60)
+cat.blit(cat_left_leg, (120, 80))
+
+ellipse(cat_right_leg, (255, 130, 71), (0, 0, 60, 15))
+cat_right_leg = pygame.transform.rotate(cat_right_leg, -30)
+cat.blit(cat_right_leg, (130, 80))
+
+ellipse(cat_tail, (255, 130, 71), (0, 0, 80, 20))
+cat_tail = pygame.transform.rotate(cat_tail, 230)
+cat.blit(cat_tail, (130, 25))
+
+ellipse(fish, (102, 205, 170), (0, 0, 40, 10))
+polygon(fish, (178, 34, 34), [(40, 5), (50, 10), (35, 15)])
+fish = pygame.transform.rotate(fish, -20)
+cat.blit(fish, (0, 65))
 
 pygame.Surface.unlock(man)
 pygame.Surface.unlock(coat)
+pygame.Surface.unlock(cat)
 screen.blit(man, (200, 310))
 
 screen.blit(tent, (30, 70))
-screen.blit(cat, (30, 430))
+screen.blit(cat, (20, 430))
 
 
 
