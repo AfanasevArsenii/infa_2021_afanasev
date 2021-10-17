aeaae62153decbd4939f080e7e189cbbaea4cbfb
Afanasev Arsenii <afanasev.arsenii@phystech.edu>
2021-10-17 21:12:52 +0300

    Добавил функцию подсчёта попаданий в шарики.

--- lab3/6.1.py
+++ lab3/6.1.py
@@ -3,9 +3,11 @@ from pygame.draw import *
 from random import randint
 pygame.init()
 
-FPS = 1
-screen = pygame.display.set_mode((800, 800))
+FPS = 30
+screen = pygame.display.set_mode((900, 600))
 score = 0  # переменная для подсчёта очков
+balls_quantity = 5
+balls = []
 
 #  объявляем базовые цвета шариков
 RED = (255, 0, 0)
@@ -18,14 +20,29 @@ BLACK = (0, 0, 0)
 COLORS = [RED, BLUE, YELLOW, GREEN, MAGENTA, CYAN]
 
 
-def new_ball():
+def new_balls():
     """Функция рисует новый шарик."""
-    global x, y, r
-    x = randint(100, 700)
-    y = randint(100, 700)
-    r = randint(10, 100)
-    color = COLORS[randint(0, 5)]
-    circle(screen, color, (x, y), r)
+    for i in range (balls_quantity):
+        x = randint(100, 800)
+        y = randint(100, 500)
+        r = randint(1, 100)
+        color = COLORS[randint(0, 5)]
+        dx = randint(-10, 10)
+        dy = randint(-10, 10)
+        balls.append([x, y, r, color, dx, dy])
+
+
+
+def move_ball():
+    """Функция двигает шарик"""
+    for i in range(balls_quantity):
+        balls[i][0] = balls[i][0] + balls[i][4]
+        balls[i][1] += balls[i][5]
+        if balls[i][0] - balls[i][2] < 0 or balls[i][0] + balls[i][2] > 900:
+            balls[i][4] = - balls[i][4]
+        if balls[i][1] - balls[i][2] < 0 or balls[i][1] + balls[i][2] > 600:
+            balls[i][5] = - balls[i][5]
+        circle(screen, balls[i][3], (balls[i][0], balls[i][1]), balls[i][2])
 
 
 def draw_score(score):
@@ -38,6 +55,24 @@ def draw_score(score):
     screen.blit(text, (250, 50))
 
 
+def click(cur_event):
+    """
+    Функция реагирует на нажатие на шарик.
+    Если попал - прибавляется балл, если нет - балл отнимается.
+    """
+    hasClicked = False
+    for i in range(balls_quantity):
+        global score
+        if (cur_event.pos[0]-balls[i][0])**2 + (cur_event.pos[1]-balls[i][1])**2 <= balls[i][2]**2:
+            score += 1
+            hasClicked = True
+    if not hasClicked:
+        score -= 1
+        print('Miss again!')
+
+
+new_balls()
+
 pygame.display.update()
 clock = pygame.time.Clock()
 finished = False
@@ -48,13 +83,8 @@ while not finished:
         if event.type == pygame.QUIT:
             finished = True
         elif event.type == pygame.MOUSEBUTTONDOWN:
-            if (event.pos[0]-x)**2 + (event.pos[1]-y)**2 <= r**2:  # проверяем, поймали ли шарик
-                print('Yeah, boy!')
-                score += 1  # добавляем очки
-            else:
-                print('Miss again!')
-
-    new_ball()
+            click(event)
+    move_ball()
     draw_score(score)
     pygame.display.update()
     screen.fill(BLACK)
