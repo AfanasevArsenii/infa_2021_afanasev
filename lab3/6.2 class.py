15406b616babe0da8c423a400161e5351157c1f8
Afanasev Arsenii <afanasev.arsenii@phystech.edu>
2021-10-26 13:44:42 +0300

    Сделал движение шаров, добавил реагирование на нажатие на шар. Добавил сортировку результатов и вывод в файл results.Classes.txt

--- lab3/6.2 class.py
+++ lab3/6.2 class.py
@@ -1,15 +1,13 @@
 import pygame
 from pygame.draw import *
 from random import randint
-pygame.init()
 
-FPS = 50
-screen = pygame.display.set_mode((900, 600))
 score = 0  # переменная для подсчёта очков
 balls_quantity = 5
 rectangles_quantity = 5
-my_balls = [0]*balls_quantity
-my_rectangles = [0]*rectangles_quantity
+
+my_balls = []
+my_rectangles = []
 
 RED = (255, 0, 0)
 BLUE = (0, 0, 255)
@@ -19,6 +17,7 @@ MAGENTA = (255, 0, 255)
 CYAN = (0, 255, 255)
 BLACK = (0, 0, 0)
 COLORS = [RED, BLUE, YELLOW, GREEN, MAGENTA, CYAN]
+
 
 class Balls:
     """Класс шариков
@@ -79,7 +78,6 @@ class Rectangles:
         self.dy = randint(-10, 10)
         self.screen = screen
 
-
     def move(self):
         """Функция двигает прямоуголник (меняет координаты)
         Если прямоугольник ударился о стенку меняет скорость прямоугольника"""
@@ -94,19 +92,90 @@ class Rectangles:
         if self.y + self.height > 600:
             self.dy = randint(-10, -1)
 
-
     def draw(self):
         """Функция отрисовывает новое положение шарика"""
         rect(self.screen, self.color, (self.x, self.y, self.width, self.height))
 
 
+def draw_score(score):
+    """
+    Функция рисует панель счётчика с обновляющимся счётом:
+    :param score: счёт (количество пойманных фигур)
+    """
+    f = pygame.font.SysFont('arial', 36)
+    text = f.render('Score: ' + str(score), True, (255, 255, 255))
+    screen.blit(text, (250, 50))
+
+
+def click(cur_event):
+    global my_rectangles
+    """
+    Функция реагирует на нажатие на шарик или на прямоугольник.
+    Если попал в шарик - прибавляется балл, если попал в прямоугольник - 3 балла,
+    если нет - балл отнимается.
+    :param cur_event: событие (клик)
+    """
+    clicked = False
+    global score
+    for i in range(len(my_balls)):
+        if (cur_event.pos[0]-my_balls[i].x)**2 + (cur_event.pos[1]-my_balls[i].y)**2 <= my_balls[i].r**2:
+            score += 1
+            my_balls.pop(i)
+            my_balls.append(Balls(screen))
+            clicked = True
+    for i in range(len(my_rectangles)):
+        if (cur_event.pos[0] >= my_rectangles[i].x)\
+                and (cur_event.pos[0] <= my_rectangles[i].x+my_rectangles[i].width)\
+                and (cur_event.pos[1] >= my_rectangles[i].y)\
+                and (cur_event.pos[1] <= my_rectangles[i].y + my_rectangles[i].height):
+            score += 3
+            my_rectangles.pop(i)
+            my_rectangles.append(Rectangles(screen))
+            clicked = True
+    if not clicked:
+        score -= 1
+
+
+def custom_key(k):
+    return k[1]
+
+
+def results_table():
+    results = []
+    with open("resultsClasses.txt", 'r') as output:
+        records = output.readlines()
+
+    if len(records) == 0:
+        with open("resultsClasses.txt", 'w') as output:
+            output.write(name + ": " + str(score) + '\n')
+    else:
+        names = list(map(lambda x: x.split(': ')[0], records))
+        scores = list(map(lambda x: x.split(': ')[1], records))
+        scores = list(map(lambda x: x.split('\n')[0], scores))
+        for i in range(len(records)):
+            results.append([names[i], int(scores[i])])
+        results.append([name, score])
+        results.sort(key=custom_key, reverse=True)
+        output = open("resultsClasses.txt", 'w')
+        for i in range(len(results)):
+            output.write(results[i][0] + ": " + str(results[i][1]) + '\n')
+        output.close()
+
+
+print("Введите своё имя: ")
+name = input()
+
+pygame.init()
+
+FPS = 50
+screen = pygame.display.set_mode((900, 600))
 
 for i in range(balls_quantity):  # создаём заданное количество шариков
-    my_balls[i] = Balls(screen)
+    my_balls.append(Balls(screen))
+
 
 for i in range(rectangles_quantity):  # создаём заданное количество прямоугольников
-    my_rectangles[i] = Rectangles(screen)
-
+    my_rectangles.append(Rectangles(screen))
 
 
 pygame.display.update()
@@ -118,16 +187,17 @@ while not finished:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             finished = True
-        #elif event.type == pygame.MOUSEBUTTONDOWN:
-            #click(event)
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            click(event)
     for i in range(balls_quantity):  # двигаем шарики
         my_balls[i].move()
         my_balls[i].draw()
     for i in range(rectangles_quantity):  # двигаем прямоугольники
         my_rectangles[i].move()
         my_rectangles[i].draw()
-    #draw_score(score)
+    draw_score(score)
     pygame.display.update()
     screen.fill(BLACK)
 
+results_table()
 pygame.quit()
